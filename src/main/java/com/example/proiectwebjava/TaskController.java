013d0cf5cbeb47d22cf6a2c4a48027862d4af6fc
CristianAnton <cristian.anton99@yahoo.com>
2022-01-14 18:49:44 +0200

    adaugat @Rest controller si modificat din functia de DELETE saveTask in deleteTask

--- src/main/java/com/example/proiectwebjava/TaskController.java
+++ src/main/java/com/example/proiectwebjava/TaskController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@RestController
 @RequestMapping("/api")
 public class TaskController {
 
@@ -59,7 +60,7 @@ public class TaskController {
         Optional<Task> existingTask = taskService.findTaskById(id);
         if(existingTask.isPresent())
         {
-            taskService.saveTask(existingTask.get());
+            taskService.deleteTask(existingTask.get());
             return ResponseEntity.noContent().build();
         }
         else { return ResponseEntity.notFound().build(); }
--- src/test/java/com/example/proiectwebjava/CompanyControllerTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.proiectwebjava;
-
-public class CompanyControllerTest {
-}
